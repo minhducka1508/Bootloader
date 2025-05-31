22af2f68498c3b01712ed0713ba69e9516cc7739
Hihi Trần Minh Đức <minhducka1508@gmail.com>
2025-05-31 16:47:38 +0700

    Feat: Creat file bootloader

---
+++ Bootloader/Bootloader.c
@@ -0,0 +1,5 @@
+/*
+ *	Bootloader.c
+ *	Created on: May 31, 2025
+ *		Author: Trần Minh Đức
+ */
---
+++ Bootloader/Bootloader.h
@@ -0,0 +1,19 @@
+/*
+ *	Bootloader.h
+ *	Created on: May 31, 2025
+ *		Author: Trần Minh Đức
+ */
+
+#ifndef _BOOTLOADER_H_
+#define _BOOTLOADER_H_
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _BOOTLOADER_H_ */
