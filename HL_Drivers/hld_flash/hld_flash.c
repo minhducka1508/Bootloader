40d333813dac619891d37bfa605ace4a39db6d65
Trần Minh Đức <minhducka1508@gmail.com>
2025-06-04 23:43:13 +0700

    Feat: Add HLD_Flash

---
+++ HL_Drivers/hld_flash/hld_flash.c
@@ -0,0 +1,152 @@
+#include "hld_flash.h"
+#include "stm32f4xx_hal.h"
+
+/* Private defines */
+#define FLASHIF_OK 0
+#define FLASHIF_ERASEKO 1
+#define FLASHIF_WRITING_ERROR 2
+#define FLASHIF_WRITINGCTRL_ERROR 3
+#define FLASHIF_PROTECTION_ERRROR 4
+#define FLASHIF_PROTECTION_NONE 0
+#define FLASHIF_PROTECTION_WRPENABLED 1
+
+/**
+ * @brief  Unlocks Flash for write access
+ */
+void FLASH_If_Init(void)
+{
+	HAL_FLASH_Unlock();
+	__HAL_FLASH_CLEAR_FLAG(FLASH_FLAG_EOP | FLASH_FLAG_OPERR | FLASH_FLAG_WRPERR | FLASH_FLAG_PGAERR | FLASH_FLAG_PGPERR | FLASH_FLAG_PGSERR);
+	HAL_FLASH_Lock();
+}
+
+/**
+ * @brief  Erases the flash from start address to end address
+ * @param  start: start address to erase from
+ */
+uint32_t FLASH_If_Erase(uint32_t start)
+{
+	HAL_StatusTypeDef status;
+	uint32_t FirstSector, NbOfSectors, SectorError;
+	FLASH_EraseInitTypeDef EraseInitStruct;
+
+	HAL_FLASH_Unlock();
+
+	FirstSector = GetSector(start);
+	NbOfSectors = GetSector(APP_END_ADDR) - FirstSector + 1;
+
+	EraseInitStruct.TypeErase = FLASH_TYPEERASE_SECTORS;
+	EraseInitStruct.VoltageRange = FLASH_VOLTAGE_RANGE_3;
+	EraseInitStruct.Sector = FirstSector;
+	EraseInitStruct.NbSectors = NbOfSectors;
+
+	status = HAL_FLASHEx_Erase(&EraseInitStruct, &SectorError);
+
+	HAL_FLASH_Lock();
+
+	return (status == HAL_OK) ? FLASHIF_OK : FLASHIF_ERASEKO;
+}
+
+/**
+ * @brief  Writes data to flash
+ */
+uint32_t FLASH_If_Write(uint32_t destination, uint32_t *p_source, uint32_t length)
+{
+	uint32_t i;
+
+	HAL_FLASH_Unlock();
+
+	for (i = 0; i < length; i++)
+	{
+		if (HAL_FLASH_Program(FLASH_TYPEPROGRAM_WORD, destination, p_source[i]) == HAL_OK)
+		{
+			if (*(uint32_t *)destination != p_source[i])
+			{
+				HAL_FLASH_Lock();
+				return FLASHIF_WRITINGCTRL_ERROR;
+			}
+			destination += 4;
+		}
+		else
+		{
+			HAL_FLASH_Lock();
+			return FLASHIF_WRITING_ERROR;
+		}
+	}
+
+	HAL_FLASH_Lock();
+	return FLASHIF_OK;
+}
+
+/**
+ * @brief  Returns the write protection status.
+ */
+uint32_t FLASH_If_GetWriteProtectionStatus(void)
+{
+	FLASH_OBProgramInitTypeDef OBInit;
+	uint32_t ProtectedSectors = FLASHIF_PROTECTION_NONE;
+
+	HAL_FLASH_Unlock();
+	HAL_FLASH_OB_Unlock();
+
+	HAL_FLASHEx_OBGetConfig(&OBInit);
+
+	HAL_FLASH_OB_Lock();
+	HAL_FLASH_Lock();
+
+	if ((~OBInit.WRPSector) & FLASH_SECTORS_TO_BE_PROTECTED)
+	{
+		ProtectedSectors = FLASHIF_PROTECTION_WRPENABLED;
+	}
+
+	return ProtectedSectors;
+}
+
+/**
+ * @brief  Configures write protection
+ */
+uint32_t FLASH_If_WriteProtectionConfig(uint32_t protectionstate)
+{
+	FLASH_OBProgramInitTypeDef OBInit;
+	HAL_StatusTypeDef result;
+
+	HAL_FLASH_Unlock();
+	HAL_FLASH_OB_Unlock();
+
+	HAL_FLASHEx_OBGetConfig(&OBInit);
+
+	OBInit.OptionType = OPTIONBYTE_WRP;
+	OBInit.WRPSector = FLASH_SECTORS_TO_BE_PROTECTED;
+	OBInit.WRPState = (protectionstate == FLASHIF_PROTECTION_WRPENABLED) ? OB_WRPSTATE_ENABLE : OB_WRPSTATE_DISABLE;
+
+	result = HAL_FLASHEx_OBProgram(&OBInit);
+
+	HAL_FLASH_OB_Launch(); // apply new OBs
+	HAL_FLASH_OB_Lock();
+	HAL_FLASH_Lock();
+
+	return (result == HAL_OK) ? FLASHIF_OK : FLASHIF_PROTECTION_ERRROR;
+}
+
+/**
+ * @brief  Maps address to flash sector
+ */
+uint32_t GetSector(uint32_t Address)
+{
+	if (Address < 0x08004000)
+		return FLASH_SECTOR_0;
+	else if (Address < 0x08008000)
+		return FLASH_SECTOR_1;
+	else if (Address < 0x0800C000)
+		return FLASH_SECTOR_2;
+	else if (Address < 0x08010000)
+		return FLASH_SECTOR_3;
+	else if (Address < 0x08020000)
+		return FLASH_SECTOR_4;
+	else if (Address < 0x08040000)
+		return FLASH_SECTOR_5;
+	else if (Address < 0x08060000)
+		return FLASH_SECTOR_6;
+	else
+		return FLASH_SECTOR_7;
+}
---
+++ HL_Drivers/hld_flash/hld_flash.h
@@ -0,0 +1,77 @@
+/**
+  ******************************************************************************
+  * @file    hld_flash.h
+  * @brief   This file provides all the headers of the flash_if functions,
+  *          adapted for STM32F4 (sector-based flash layout).
+  ******************************************************************************
+  */
+
+#ifndef _HLD_FLASH_H_
+#define _HLD_FLASH_H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32f4xx_hal.h"
+
+/* Exported constants --------------------------------------------------------*/
+
+/* Error code */
+enum 
+{
+  FLASHIF_OK = 0,
+  FLASHIF_ERASEKO,
+  FLASHIF_WRITINGCTRL_ERROR,
+  FLASHIF_WRITING_ERROR,
+  FLASHIF_PROTECTION_ERRROR
+};
+
+/* Protection types */
+enum {
+  FLASHIF_PROTECTION_NONE         = 0x00,
+  FLASHIF_PROTECTION_WRPENABLED   = 0x01
+};
+
+/* Write protection config states */
+enum {
+  FLASHIF_WRP_ENABLE,
+  FLASHIF_WRP_DISABLE
+};
+
+/* Flash layout for STM32F411RE (512KB, sectors 0–7)
+ *   - Sector 0: 16 KB  @ 0x08000000
+ *   - Sector 1: 16 KB  @ 0x08004000
+ *   - Sector 2: 16 KB  @ 0x08008000
+ *   - Sector 3: 16 KB  @ 0x0800C000
+ *   - Sector 4: 64 KB  @ 0x08010000
+ *   - Sector 5: 128 KB @ 0x08020000
+ *   - Sector 6: 128 KB @ 0x08040000
+ *   - Sector 7: 128 KB @ 0x08060000
+ */
+
+/* Define the start address where user app will be loaded (must align with a sector boundary) */
+#define APP_START_ADDR 0x08008000
+//#define APP2_START_ADDR 0x08008000
+
+#define APP_FLASH_SIZE ((uint32_t)(248 * 1024))
+
+#define APP_END_ADDR ((uint32_t)(APP_START_ADDR + APP_FLASH_SIZE - 1))
+
+/* Bitmap of sectors to protect (example: sectors 2–7 = bits 2 to 7) */
+#define FLASH_SECTORS_TO_BE_PROTECTED  ((uint32_t)(0x000000FC))  // sectors 2 to 7
+
+/* Exported functions --------------------------------------------------------*/
+void     FLASH_If_Init(void);
+uint32_t FLASH_If_Erase(uint32_t start_address);
+uint32_t FLASH_If_Write(uint32_t destination, uint32_t *p_source, uint32_t length);
+uint32_t FLASH_If_GetWriteProtectionStatus(void);
+uint32_t FLASH_If_WriteProtectionConfig(uint32_t protectionstate);
+uint32_t GetSector(uint32_t Address);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _HLD_FLASH_H_ */
